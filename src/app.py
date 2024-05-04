3152ab800637d7f20f296472f97599dd7cfd363b
Tristan Simpson <heytristaann@gmail.com>
2024-05-04 10:12:22 -0400

    c

---
+++ src/app.py
@@ -0,0 +1,11 @@
+##
+## Web app that uses streamlit and the gemini api
+##
+## The user can enter a prompt and the gemini model will
+## return a response (then output it to the screen)
+##
+
+## Step 1: Create the Gemini API key
+
+## Step 2: Install the required libraries and import here
+## pip install streamlit
